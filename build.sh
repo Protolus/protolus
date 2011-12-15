f594b2ca852413a3af57ca4be7bc028ae5b923ec
khrome <abbey@khrome.net>
2011-12-15 11:35:28 -0800

    Build script update

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
-dr=`dirname "$0"`
+
+#init
+dr=`pwd`
 name=$2
 moduleList=$1
-if [$moduleList -eq '']; then
-    moduleList="node,extensions,core"
-fi
 header=""
 footer=""
 fileList=""
 type="client"
 
-#split all the module names
+#default module list
+if [$moduleList -eq '']; then
+    moduleList="node,extensions,core"
+fi
+
+#split all the module names, accumulate the required files
 old_IFS=${IFS}
 IFS=","
 for module in $moduleList; do
@@ -41,13 +45,13 @@ if (typeof exports != 'undefined') (function(){
             fileList=$fileList"Extensions/Array.extensions,Extensions/Element.extensions,Extensions/NodeList.extensions,Extensions/Function.extensions,Extensions/Number.extensions,Extensions/Object.extensions,Extensions/Request.Pool,Extensions/Request.Stable,Extensions/String.extensions,"
         ;;
         image)
-            fileList=$fileList"Image/Protolus.Image.Booth,Image/Protolus.Image,Image/Protolus.Image.Layer,Image/Protolus.Image.Filter,Image/Protolus.Image.Tool,Image/Protolus.Image.Brush,Image/Protolus.Image.Operation,Image/Brushes/Protolus.Image.Brush.10pxScatter.js,Image/Brushes/Protolus.Image.Brush.3pxRound.js,Image/Brushes/Protolus.Image.Brush.10pxSoftRound.js,Image/Brushes/Protolus.Image.Brush.40pxSoftRound.js,Image/Brushes/Protolus.Image.Brush.15pxSoftRound.js,Image/Brushes/Protolus.Image.Brush.5pxRound.js,Image/Brushes/Protolus.Image.Brush.1pxSquare.js,Image/Brushes/Protolus.Image.Brush.5pxSoftRound.js,Image/Brushes/Protolus.Image.Brush.20pxSoftRound.js,Image/Brushes/Protolus.Image.Brush.5pxSquare.js,Image/Filters/Protolus.Image.Filter.Emboss.js,Image/Filters/Protolus.Image.Filter.Laplacian.js,Image/Filters/Protolus.Image.Filter.GaussianBlur.js,Image/Filters/Protolus.Image.Filter.Sharpen.js,Image/Filters/Protolus.Image.Filter.HighPass.js,Image/Filters/Protolus.Image.Filter.Sobel.js,Image/Tools/Protolus.Image.Tool.Clone.js,Image/Tools/Protolus.Image.Tool.Paintbrush.js,Image/Tools/Protolus.Image.Tool.Eraser.js,Image/Tools/Protolus.Image.Tool.Paintbucket.js,Image/Tools/Protolus.Image.Tool.Eyedropper.js,Image/Operations/Protolus.Image.Operation.BrightnessContrast.js,Image/Operations/Protolus.Image.Operation.Negative.js,"
+            fileList=$fileList"Image/Protolus.Image.Booth,Image/Protolus.Image,Image/Protolus.Image.Layer,Image/Protolus.Image.Filter,Image/Protolus.Image.Tool,Image/Protolus.Image.Brush,Image/Protolus.Image.Operation,Image/Brushes/Protolus.Image.Brush.10pxScatter,Image/Brushes/Protolus.Image.Brush.3pxRound,Image/Brushes/Protolus.Image.Brush.10pxSoftRound,Image/Brushes/Protolus.Image.Brush.40pxSoftRound,Image/Brushes/Protolus.Image.Brush.15pxSoftRound,Image/Brushes/Protolus.Image.Brush.5pxRound,Image/Brushes/Protolus.Image.Brush.1pxSquare,Image/Brushes/Protolus.Image.Brush.5pxSoftRound,Image/Brushes/Protolus.Image.Brush.20pxSoftRound,Image/Brushes/Protolus.Image.Brush.5pxSquare,Image/Filters/Protolus.Image.Filter.Emboss,Image/Filters/Protolus.Image.Filter.Laplacian,Image/Filters/Protolus.Image.Filter.GaussianBlur,Image/Filters/Protolus.Image.Filter.Sharpen,Image/Filters/Protolus.Image.Filter.HighPass,Image/Filters/Protolus.Image.Filter.Sobel,Image/Tools/Protolus.Image.Tool.Clone,Image/Tools/Protolus.Image.Tool.Paintbrush,Image/Tools/Protolus.Image.Tool.Eraser,Image/Tools/Protolus.Image.Tool.Paintbucket,Image/Tools/Protolus.Image.Tool.Eyedropper,Image/Operations/Protolus.Image.Operation.BrightnessContrast,Image/Operations/Protolus.Image.Operation.Negative,"
         ;;
         game)
             fileList=$fileList""
         ;;
         parsers)
-            fileList=$fileList"Parsers/Protolus.TagParser,Protolus.Template,Parsers/Protolus.HTMLParser,Templating/Protolus.Template.Smarty"
+            fileList=$fileList"Parsers/Protolus.TagParser,Protolus.Template,Parsers/Protolus.HTMLParser,Templating/Protolus.Template.Smarty,"
         ;;
     esac
 done
@@ -60,14 +64,15 @@ if [$2 -eq '']; then
 fi
 filename="Build/$name"
 
-echo "Built "$moduleList" for "$type" as "$filename
-
 #output to file 
 echo $header >> $filename
 old_IFS=${IFS}
 IFS=","
 for v in $fileList; do
-    cat "Source/$dr/$v.js" >>$filename
+    cat "$dr/Source/$v.js" >>$filename
 done
 IFS=${old_IFS}
 echo $footer >> $filename
+
+#dump the results
+echo "Built "$moduleList" for "$type" as "$filename
